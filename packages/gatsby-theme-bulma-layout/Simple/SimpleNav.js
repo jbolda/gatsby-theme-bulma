e0615ad03499b33bd1679cf0cbaecf798add1266
Jacob Bolda <jbolda@gmail.com>
2018-02-19 07:01:41 -0600

    add babel

--- packages/gatsby-theme-bulma-layout/Simple/SimpleNav.js
+++ packages/gatsby-theme-bulma-layout/Simple/SimpleNav.js
@@ -1,39 +1,109 @@
-import React from "react"
-import MasterLayout from 'gatsby-theme-bulma/MasterLayout'
-import SimpleTopNav from './components/SimpleTopNav'
-import heartData from './assets/heart-white.svg'
+'use strict';
 
-class SimpleLayout extends React.Component {
-  render() {
-    let {sitemetadata} = this.props
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-    return (
-      <MasterLayout sitemetadata={sitemetadata} id="SimpleLayout">
-        <SimpleTopNav sitemetadata={sitemetadata} location={this.props.location}/>
-        {this.props.children}
-        <section className="footer">
-          <div className="container content has-text-centered">
-            <p className="copyright">&copy; All rights reserved.</p>
-            <p className="copyright">
-              Made with <Heart icon={heartData} alt="heart"/> by{` `}
-              <a className="copyright" href={sitemetadata.siteTwitterUrl}>Jacob Bolda</a>
-            </p>
-          </div>
-        </section>
-      </MasterLayout>
-    )
+var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _MasterLayout = require('gatsby-theme-bulma/MasterLayout');
+
+var _MasterLayout2 = _interopRequireDefault(_MasterLayout);
+
+var _SimpleTopNav = require('./components/SimpleTopNav');
+
+var _SimpleTopNav2 = _interopRequireDefault(_SimpleTopNav);
+
+var _heartWhite = require('./assets/heart-white.svg');
+
+var _heartWhite2 = _interopRequireDefault(_heartWhite);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SimpleLayout = function (_React$Component) {
+  (0, _inherits3.default)(SimpleLayout, _React$Component);
+
+  function SimpleLayout() {
+    (0, _classCallCheck3.default)(this, SimpleLayout);
+    return (0, _possibleConstructorReturn3.default)(this, (SimpleLayout.__proto__ || (0, _getPrototypeOf2.default)(SimpleLayout)).apply(this, arguments));
   }
-}
 
-const Heart = ({ icon, alt }) => (
-  <img
-  src={icon}
-  alt={alt}
-  style={{
-    height: `25px`,
-    marginBottom: `-7px`
-  }}
-  />
-)
+  (0, _createClass3.default)(SimpleLayout, [{
+    key: 'render',
+    value: function render() {
+      var sitemetadata = this.props.sitemetadata;
 
-export default SimpleLayout
+
+      return _react2.default.createElement(
+        _MasterLayout2.default,
+        { sitemetadata: sitemetadata, id: 'SimpleLayout' },
+        _react2.default.createElement(_SimpleTopNav2.default, { sitemetadata: sitemetadata, location: this.props.location }),
+        this.props.children,
+        _react2.default.createElement(
+          'section',
+          { className: 'footer' },
+          _react2.default.createElement(
+            'div',
+            { className: 'container content has-text-centered' },
+            _react2.default.createElement(
+              'p',
+              { className: 'copyright' },
+              '\xA9 All rights reserved.'
+            ),
+            _react2.default.createElement(
+              'p',
+              { className: 'copyright' },
+              'Made with ',
+              _react2.default.createElement(Heart, { icon: _heartWhite2.default, alt: 'heart' }),
+              ' by',
+              ' ',
+              _react2.default.createElement(
+                'a',
+                { className: 'copyright', href: sitemetadata.siteTwitterUrl },
+                'Jacob Bolda'
+              )
+            )
+          )
+        )
+      );
+    }
+  }]);
+  return SimpleLayout;
+}(_react2.default.Component);
+
+var Heart = function Heart(_ref) {
+  var icon = _ref.icon,
+      alt = _ref.alt;
+  return _react2.default.createElement('img', {
+    src: icon,
+    alt: alt,
+    style: {
+      height: '25px',
+      marginBottom: '-7px'
+    }
+  });
+};
+
+exports.default = SimpleLayout;
--- packages/gatsby-theme-bulma-layout/Simple/components/SimpleTopNav.js
+++ packages/gatsby-theme-bulma-layout/Simple/components/SimpleTopNav.js
@@ -1,49 +1,108 @@
-import React from "react";
-import Link from "gatsby-link";
-import styles from "../../palette.json";
+"use strict";
 
-class SimpleTopNav extends React.Component {
-  render() {
-    return (
-      <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link
-            to="/"
-            className="navbar-item"
-            style={this.props.location.pathname == '/' ? {backgroundColor: styles.colors.P5} : {}}
-            >
-            <span
-              className="title"
-              style={this.props.location.pathname == '/' ? {color: styles.colors.P2} : {color: styles.colors.P5}}
-              >
-              B
-            </span>
-          </Link>
-          <Link
-            to="/"
-            className="navbar-item"
-            style={this.props.location.pathname == '/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
-            >
-            Articles
-          </Link>
-          <Link
-            to="/about/"
-            className="navbar-item"
-            style={this.props.location.pathname == '/about/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
-            >
-            About
-          </Link>
-          <Link
-            to="/contact/"
-            className="navbar-item"
-            style={this.props.location.pathname == '/contact/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
-            >
-            Contact
-          </Link>
-        </div>
-      </nav>
-    )
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _getPrototypeOf = require("babel-runtime/core-js/object/get-prototype-of");
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require("babel-runtime/helpers/classCallCheck");
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require("babel-runtime/helpers/createClass");
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = require("babel-runtime/helpers/possibleConstructorReturn");
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require("babel-runtime/helpers/inherits");
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _gatsbyLink = require("gatsby-link");
+
+var _gatsbyLink2 = _interopRequireDefault(_gatsbyLink);
+
+var _palette = require("../../palette.json");
+
+var _palette2 = _interopRequireDefault(_palette);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SimpleTopNav = function (_React$Component) {
+  (0, _inherits3.default)(SimpleTopNav, _React$Component);
+
+  function SimpleTopNav() {
+    (0, _classCallCheck3.default)(this, SimpleTopNav);
+    return (0, _possibleConstructorReturn3.default)(this, (SimpleTopNav.__proto__ || (0, _getPrototypeOf2.default)(SimpleTopNav)).apply(this, arguments));
   }
-}
 
-export default SimpleTopNav;
+  (0, _createClass3.default)(SimpleTopNav, [{
+    key: "render",
+    value: function render() {
+      return _react2.default.createElement(
+        "nav",
+        { className: "navbar is-fixed-top", role: "navigation", "aria-label": "main navigation" },
+        _react2.default.createElement(
+          "div",
+          { className: "navbar-brand" },
+          _react2.default.createElement(
+            _gatsbyLink2.default,
+            {
+              to: "/",
+              className: "navbar-item",
+              style: this.props.location.pathname == '/' ? { backgroundColor: _palette2.default.colors.P5 } : {}
+            },
+            _react2.default.createElement(
+              "span",
+              {
+                className: "title",
+                style: this.props.location.pathname == '/' ? { color: _palette2.default.colors.P2 } : { color: _palette2.default.colors.P5 }
+              },
+              "B"
+            )
+          ),
+          _react2.default.createElement(
+            _gatsbyLink2.default,
+            {
+              to: "/",
+              className: "navbar-item",
+              style: this.props.location.pathname == '/' ? { backgroundColor: _palette2.default.colors.P5, color: _palette2.default.colors.P2 } : {}
+            },
+            "Articles"
+          ),
+          _react2.default.createElement(
+            _gatsbyLink2.default,
+            {
+              to: "/about/",
+              className: "navbar-item",
+              style: this.props.location.pathname == '/about/' ? { backgroundColor: _palette2.default.colors.P5, color: _palette2.default.colors.P2 } : {}
+            },
+            "About"
+          ),
+          _react2.default.createElement(
+            _gatsbyLink2.default,
+            {
+              to: "/contact/",
+              className: "navbar-item",
+              style: this.props.location.pathname == '/contact/' ? { backgroundColor: _palette2.default.colors.P5, color: _palette2.default.colors.P2 } : {}
+            },
+            "Contact"
+          )
+        )
+      );
+    }
+  }]);
+  return SimpleTopNav;
+}(_react2.default.Component);
+
+exports.default = SimpleTopNav;

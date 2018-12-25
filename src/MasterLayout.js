bdec30a2fcfd74c6d203663a1a08202d61305a75
Jacob Bolda <me@jacobbolda.com>
2018-12-24 23:27:58 -0600

    update styles and packages

--- src/MasterLayout.js
+++ src/MasterLayout.js
@@ -1,24 +1,35 @@
-import React from "react"
-import Helmet from "react-helmet"
-import "./css/base.scss"
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import "./css/base.scss";
 
 class MasterLayout extends React.Component {
   render() {
-    let {sitemetadata} = this.props
+    let { siteMetadata } = this.props;
 
     return (
       <div className="MasterLayout is-light">
         <Helmet
-          defaultTitle={sitemetadata.siteTitle}
-          title={sitemetadata.siteTitle}
+          defaultTitle={siteMetadata.siteTitle}
+          title={siteMetadata.siteTitle}
           meta={[
-            { name: `description`, content: sitemetadata.siteDescr },
-            { name: `keywords`, content: `articles` },
+            { name: `description`, content: siteMetadata.siteDescription },
+            { name: `keywords`, content: `articles` }
           ]}
-        />
+        >
+          <html className="has-navbar-fixed-top" lang="en" />
+        </Helmet>
+        {this.props.children}
       </div>
-    )
+    );
   }
 }
 
-export default MasterLayout
+export default MasterLayout;
+
+MasterLayout.propTypes = {
+  siteMetadata: PropTypes.shape({
+    siteTitle: PropTypes.string.isRequired,
+    siteDescription: PropTypes.string.isRequired
+  }).isRequired
+};

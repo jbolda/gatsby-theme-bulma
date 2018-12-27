344dbd4fd07c8941d13ba375f5174a45821ae149
Jacob Bolda <me@jacobbolda.com>
2018-12-27 07:25:07 -0600

    add hamburger to top nav in layout

--- packages/gatsby-theme-bulma-layout/src/components/Simple/SimpleNav.js
+++ packages/gatsby-theme-bulma-layout/src/components/Simple/SimpleNav.js
@@ -1,39 +1,53 @@
-import React from "react"
-import MasterLayout from 'gatsby-theme-bulma/MasterLayout'
-import SimpleTopNav from './components/SimpleTopNav'
-import heartData from './assets/heart-white.svg'
+import React from "react";
+import PropTypes from "prop-types";
+import MasterLayout from "gatsby-theme-bulma-core/src/components/MasterLayout";
+import SimpleTopNav from "./components/SimpleTopNav";
 
-class SimpleLayout extends React.Component {
-  render() {
-    let {sitemetadata} = this.props
+import styles from "./palette.json";
 
-    return (
-      <MasterLayout sitemetadata={sitemetadata} id="SimpleLayout">
-        <SimpleTopNav sitemetadata={sitemetadata} location={this.props.location}/>
-        {this.props.children}
-        <section className="footer">
-          <div className="container content has-text-centered">
-            <p className="copyright">&copy; All rights reserved.</p>
-            <p className="copyright">
-              Made with <Heart icon={heartData} alt="heart"/> by{` `}
-              <a className="copyright" href={sitemetadata.siteTwitterUrl}>Jacob Bolda</a>
-            </p>
-          </div>
-        </section>
-      </MasterLayout>
-    )
-  }
-}
+const SimpleNav = ({ location, site, children }) => (
+  <MasterLayout siteMetadata={site.siteMetadata} id="SimpleLayout">
+    <SimpleTopNav
+      sitemetadata={site.siteMetadata}
+      location={location}
+      textColor={styles.colors.P5}
+    />
+    {children}
+    <section className="footer">
+      <div className="container content has-text-centered">
+        <p className="copyright">
+          Made with <Heart style={{ marginBottom: `-7px` }} /> by
+          {` `}
+          <a className="copyright" href={site.siteMetadata.siteTwitterUrl}>
+            Jacob Bolda
+          </a>
+        </p>
+      </div>
+    </section>
+  </MasterLayout>
+);
 
-const Heart = ({ icon, alt }) => (
-  <img
-  src={icon}
-  alt={alt}
-  style={{
-    height: `25px`,
-    marginBottom: `-7px`
-  }}
-  />
-)
+export default SimpleNav;
 
-export default SimpleLayout
+SimpleNav.propTypes = {
+  site: PropTypes.shape({
+    siteMetadata: PropTypes.shape({
+      siteTwitterUrl: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
+};
+
+const Heart = ({ height, width, fill, style }) => (
+  <svg
+    height={height}
+    width={width}
+    fill={fill}
+    style={style}
+    viewBox="0 0 1792 1792"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M1664 596q0-81-21.5-143t-55-98.5-81.5-59.5-94-31-98-8-112 25.5-110.5 64-86.5 72-60 61.5q-18 22-49 22t-49-22q-24-28-60-61.5t-86.5-72-110.5-64-112-25.5-98 8-94 31-81.5 59.5-55 98.5-21.5 143q0 168 187 355l581 560 580-559q188-188 188-356zm128 0q0 221-229 450l-623 600q-18 18-44 18t-44-18l-624-602q-10-8-27.5-26t-55.5-65.5-68-97.5-53.5-121-23.5-138q0-220 127-344t351-124q62 0 126.5 21.5t120 58 95.5 68.5 76 68q36-36 76-68t95.5-68.5 120-58 126.5-21.5q224 0 351 124t127 344z" />
+  </svg>
+);
+
+Heart.defaultProps = { height: "25px", width: "25px", fill: "#F4F4F4" };
--- packages/gatsby-theme-bulma-layout/src/components/Simple/components/SimpleTopNav.js
+++ packages/gatsby-theme-bulma-layout/src/components/Simple/components/SimpleTopNav.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Link from "gatsby-link";
-import styles from "../../palette.json";
+import styles from "../palette.json";
 
 class SimpleTopNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleHamburgerMenu = this.toggleHamburgerMenu.bind(this);
+    this.state = {
+      hamburgerActive: false
+    };
+  }
+
+  toggleHamburgerMenu() {
+    this.setState(prevState => {
+      return { hamburgerActive: !prevState.hamburgerActive };
+    });
+  }
+
   render() {
     return (
       <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
@@ -19,27 +33,51 @@ class SimpleTopNav extends React.Component {
               B
             </span>
           </Link>
-          <Link
-            to="/"
-            className="navbar-item"
-            style={this.props.location.pathname == '/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
-            >
-            Articles
-          </Link>
-          <Link
-            to="/about/"
-            className="navbar-item"
-            style={this.props.location.pathname == '/about/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
-            >
-            About
-          </Link>
-          <Link
-            to="/contact/"
-            className="navbar-item"
-            style={this.props.location.pathname == '/contact/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
-            >
-            Contact
-          </Link>
+          <a
+            role="button"
+            className={
+              this.state.hamburgerActive
+                ? 'navbar-burger is-active'
+                : 'navbar-burger'
+            }
+            aria-label="menu"
+            aria-expanded={this.state.hamburgerActive ? 'true' : 'false'}
+            style={{ color: styles.colors.P5 }}
+            onClick={this.toggleHamburgerMenu}
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
+        <div
+          className={
+            this.state.hamburgerActive ? 'navbar-menu is-active' : 'navbar-menu'
+          }
+        >
+          <div className="navbar-end">
+            <Link
+              to="/"
+              className="navbar-item"
+              style={this.props.location.pathname == '/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
+              >
+              Articles
+            </Link>
+            <Link
+              to="/about/"
+              className="navbar-item"
+              style={this.props.location.pathname == '/about/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
+              >
+              About
+            </Link>
+            <Link
+              to="/contact/"
+              className="navbar-item"
+              style={this.props.location.pathname == '/contact/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
+              >
+              Contact
+            </Link>
+          </div>
         </div>
       </nav>
     )

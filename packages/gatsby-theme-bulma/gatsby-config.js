75617453697e2f82a2fb2ee7a6b55f34a634b9c6
Jacob Bolda <me@jacobbolda.com>
2018-12-27 06:40:39 -0600

    update base theme to include layout and blog

--- packages/gatsby-theme-bulma/gatsby-config.js
+++ packages/gatsby-theme-bulma/gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  __experimentalThemes: [{ resolve: `gatsby-theme-bulma-core`, options: { root: __dirname } }],
+  __experimentalThemes: [
+    { resolve: `gatsby-theme-bulma-core`, options: { root: __dirname } },
+    { resolve: `gatsby-theme-bulma-layout`, options: { root: __dirname } },
+    { resolve: `gatsby-theme-bulma-blog`, options: { root: __dirname } }
+  ],
   plugins: [],
 }

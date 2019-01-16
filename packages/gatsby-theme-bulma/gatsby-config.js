36194e3a595e97ad2e25cae064f158a6c3ab75a2
Jacob Bolda <me@jacobbolda.com>
2019-01-16 07:03:35 -0600

    add -homepage to overall

--- packages/gatsby-theme-bulma/gatsby-config.js
+++ packages/gatsby-theme-bulma/gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   __experimentalThemes: [
     { resolve: `gatsby-theme-bulma-core`, options: { root: __dirname } },
     { resolve: `gatsby-theme-bulma-layout`, options: { root: __dirname } },
+    { resolve: `gatsby-theme-bulma-homepage`, options: { root: __dirname } },
     { resolve: `gatsby-theme-bulma-blog`, options: { root: __dirname } }
   ],
   plugins: [],

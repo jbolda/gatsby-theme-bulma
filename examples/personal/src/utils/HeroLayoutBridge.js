7479cf13d9dada5598e18a0170c9b2ac4f82a74c
Jacob Bolda <me@jacobbolda.com>
2019-01-26 11:01:53 -0600

    remove unneeded example files

--- examples/personal/src/utils/HeroLayoutBridge.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { StaticQuery, graphql } from "gatsby";
-import HeroLayout from "gatsby-theme-bulma-homepage/src/components/Hero/HeroLayout";
-
-const HeroLayoutBridge = props => (
-  <HeroLayout {...props}>{props.children}</HeroLayout>
-);
-
-export default HeroLayoutBridge

789782e0f1e33675474ed373a204ca2707c2236d
Jacob Bolda <me@jacobbolda.com>
2019-01-26 11:47:06 -0600

    no more utils needed in example

--- examples/personal/src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography";
-import lawtonTheme from "typography-theme-lawton";
-
-const typography = new Typography(lawtonTheme);
-
-export const { rhythm, scale } = typography;
-export default typography;

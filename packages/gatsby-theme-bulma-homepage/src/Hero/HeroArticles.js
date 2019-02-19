6808a1125852d15fcfd5aa258381d5f32d3331d4
Jacob Bolda <me@jacobbolda.com>
2019-02-19 07:54:15 -0600

    make HeroArticles work with missing images

--- packages/gatsby-theme-bulma-homepage/src/Hero/HeroArticles.js
+++ packages/gatsby-theme-bulma-homepage/src/Hero/HeroArticles.js
@@ -3,60 +3,64 @@ import { Link, StaticQuery, graphql } from 'gatsby';
 import Img from "gatsby-image";
 
 const HeroArticles = props => props.posts.edges.map(post => (
-    <div className="column is-one-third" key={post.node.frontmatter.path}>
-    <div className="card">
-    <div className="card-image">
-      <Img
-        className="image"
-        Tag="figure"
-        fluid={post.node.frontmatter.heroImage.childImageSharp.fluid}
-      />
+      <div className="column is-one-third" key={post.node.frontmatter.path}>
+      <div className="card">
+      <div className="card-image">
+        {post.node.frontmatter.heroImage && props.pictures.edges ?
+          <Img
+              className="image"
+              Tag="figure"
+              fluid={sortThroughPictures(post.node.frontmatter.heroImage, props.pictures.edges).childImageSharp.fluid}
+            />
+            :
+            null
+          }
+      </div>
+      <div className="card-content">
+          <div className="heading">
+          <div className="level">
+              <h4 className="level-left">
+              <time
+                  className="subtitle is-6"
+                  dateTime={
+                  post.node.frontmatter.updatedPretty || post.node.frontmatter.writtenPretty
+                  }
+              >
+                  {post.node.frontmatter.updatedPretty || post.node.frontmatter.writtenPretty}
+              </time>
+              </h4>
+              <h5 className="tag is-thirdary is-6 level-right">
+              {post.node.frontmatter.category}
+              </h5>
+          </div>
+          <h1 className="title">
+              <Link to={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link>
+          </h1>
+          </div>
+          <div className="content">
+          <p
+              dangerouslySetInnerHTML={{
+              __html: post.node.frontmatter.description
+              }}
+          />
+          </div>
+          <nav className="level">
+          <div className="level-left">
+              <span className="level-item">
+              <Link to={post.node.frontmatter.path}>Read</Link>
+              </span>
+          </div>
+          </nav>
+      </div>
+      </div>
     </div>
-    <div className="card-content">
-        <div className="heading">
-        <div className="level">
-            <h4 className="level-left">
-            <time
-                className="subtitle is-6"
-                dateTime={
-                post.node.frontmatter.updatedPretty || post.node.frontmatter.writtenPretty
-                }
-            >
-                {post.node.frontmatter.updatedPretty || post.node.frontmatter.writtenPretty}
-            </time>
-            </h4>
-            <h5 className="tag is-thirdary is-6 level-right">
-            {post.node.frontmatter.category}
-            </h5>
-        </div>
-        <h1 className="title">
-            <Link to={post.node.frontmatter.path}>{post.node.frontmatter.title}</Link>
-        </h1>
-        </div>
-        <div className="content">
-        <p
-            dangerouslySetInnerHTML={{
-            __html: post.node.frontmatter.description
-            }}
-        />
-        </div>
-        <nav className="level">
-        <div className="level-left">
-            <span className="level-item">
-            <Link to={post.node.frontmatter.path}>Read</Link>
-            </span>
-        </div>
-        </nav>
-    </div>
-    </div>
-  </div>
-));
+  ));
 
 export default props => (
   <StaticQuery
     query={graphql`
       query GatsbyThemeBulmaHomepageHeroArticles {
-        posts: allMarkdownRemark(filter: {fields: {sourceInstanceName: {eq: "articles"}}}, limit: 3) {
+        posts: allMarkdownRemark(filter: {fields: {sourceInstanceName: {eq: "articles"}}}, limit: 6) {
           edges {
             node {
               fields {
@@ -72,24 +76,37 @@ export default props => (
                 category
                 description
                 heroImage {
-                  childImageSharp {
-                    fluid(
-                      maxWidth: 600
-                      maxHeight: 350
-                      quality: 100
-                      cropFocus: ATTENTION
-                    ) {
-                      ...GatsbyImageSharpFluid_tracedSVG
-                    }
-                  }
+                  base
                 }
               }
               timeToRead
             }
           }
         }
+        pictures: allFile(filter: {sourceInstanceName: {eq: "articles"}, extension: {eq: "jpg"}}, limit: 6) {
+          edges {
+            node {
+              base
+              childImageSharp {
+                fluid(
+                  maxWidth: 600
+                  maxHeight: 350
+                  quality: 100
+                  cropFocus: ATTENTION
+                ) {
+                  ...GatsbyImageSharpFluid_tracedSVG
+                }
+              }
+            }
+          }
+        }
       }
     `}
-    render={queryData => <HeroArticles posts={queryData.posts} {...props} />}
+    render={queryData => <HeroArticles posts={queryData.posts} pictures={queryData.pictures} {...props} />}
   />
 );
+
+
+const sortThroughPictures = (article, pictures) => pictures.reduce(
+  (finalVal, picture) => finalVal || (article && picture.node.base === article.base ? picture.node : finalVal) 
+  , null)

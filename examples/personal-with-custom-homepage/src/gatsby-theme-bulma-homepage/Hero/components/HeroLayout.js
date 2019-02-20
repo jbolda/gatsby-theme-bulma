38b08dd5a06b32f7eedd853f927a6fa94fd39da7
Jacob Bolda <me@jacobbolda.com>
2019-02-20 08:15:07 -0600

    adjust example for new -homepage shadowing

---
+++ examples/personal-with-custom-homepage/src/gatsby-theme-bulma-homepage/Hero/components/HeroLayout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import SimpleNav from "gatsby-theme-bulma-layout/src/Simple/SimpleNav";
+import HeroLanding from './HeroLanding'
+import HeroAbout from './HeroAbout'
+
+const HeroLayout =  props => (
+  <SimpleNav location={props.location}>
+    <HeroLanding />
+    <HeroAbout />
+    {props.children}
+    <section className="section is-fourthary edge--top">
+      <h1 className="title">
+        Recipess
+      </h1>
+      <h2 className="subtitle">
+        We enjoy cooking. These are a few of our favorites eaten recently.
+      </h2>
+      <hr />
+      <div className="columns is-multiline">
+        [Insert list of recipes here]
+      </div>
+    </section>
+  </SimpleNav>
+);
+
+export default HeroLayout;
